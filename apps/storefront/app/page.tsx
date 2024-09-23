db3915e480c6bddb5e9e090c722ad1b486671912
Anas Chakroun <chakroun.anas@outlook.com>
2024-09-23 21:49:32 +0200

    Products list from SAP commerce cloud

--- apps/storefront/app/page.tsx
+++ apps/storefront/app/page.tsx
@@ -1,19 +1,21 @@
-import { SfButton } from "@storefront-ui/react";
 import { getSdk } from "../sdk/sdk.config";
-import ProductDetails from "../components/ProductDetails";
-import ProductGallery from "../components/ProductGallery";
-import ProductSlider from "../components/ProductSlider";
+import Link from "next/link";
 
 const sdk = getSdk();
 
-export default function Page() {
+export default async function Page() {
+  const { products } = await sdk.sapcc.searchProduct({});
   return (
-    <div className="flex flex-col gap-8 md:gap-12 lg:gap-16 max-w-screen-xl m-auto px-4 md:px-8 lg:px-12 xl:px-16 py-8 md:py-12 lg:py-16 xl:py-20">
-      <section className="flex flex-col items-start gap-8 md:flex-row md:gap-4 xl:gap-6">
-        <ProductGallery />
-        <ProductDetails />
-      </section>
-      <ProductSlider />
+    <div className="flex flex-col gap-3">
+      {products?.map((product) => (
+        <Link
+          href={`/product/${product.code}`}
+          key={product.code}
+          className="text-blue-500 underline"
+        >
+          - {product.name}
+        </Link>
+      ))}
     </div>
   );
 }

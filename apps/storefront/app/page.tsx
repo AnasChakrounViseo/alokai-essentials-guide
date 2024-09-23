67a22199fb310ecc3017e2d749e3e10e2bb50093
Anas Chakroun <chakroun.anas@outlook.com>
2024-09-23 21:44:14 +0200

    Storefront ProductDetails, ProductGallery and ProductSlider added.

--- apps/storefront/app/page.tsx
+++ apps/storefront/app/page.tsx
@@ -1,17 +1,19 @@
 import { SfButton } from "@storefront-ui/react";
 import { getSdk } from "../sdk/sdk.config";
+import ProductDetails from "../components/ProductDetails";
+import ProductGallery from "../components/ProductGallery";
+import ProductSlider from "../components/ProductSlider";
 
 const sdk = getSdk();
 
-export default async function Page() {
-  const { products } = await sdk.sapcc.searchProduct({});
-
-  console.log(products?.map((product) => product.name));
-
+export default function Page() {
   return (
-    <div>
-      <h1>Products</h1>
-      <SfButton>Click me</SfButton>
+    <div className="flex flex-col gap-8 md:gap-12 lg:gap-16 max-w-screen-xl m-auto px-4 md:px-8 lg:px-12 xl:px-16 py-8 md:py-12 lg:py-16 xl:py-20">
+      <section className="flex flex-col items-start gap-8 md:flex-row md:gap-4 xl:gap-6">
+        <ProductGallery />
+        <ProductDetails />
+      </section>
+      <ProductSlider />
     </div>
   );
 }
